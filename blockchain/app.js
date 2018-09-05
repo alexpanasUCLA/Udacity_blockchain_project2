08619b1da367f04a3995b261cb885d4006b84f80
Alex Panas <alexpanasucla@gmail.com>
2018-09-06 00:17:05 +0200

    add validation of user's Blockchain ID

--- blockchain/app.js
+++ blockchain/app.js
@@ -3,6 +3,11 @@ const Blockchain = require('./simpleChain.js');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const bitcoin = require('bitcoinjs-lib');
+const bitcoinMessage = require('bitcoinjs-message');
+
+
+
 const HTTP_PORT = process.env.HTTP_PORT || 8000;
 
 const app = express();
@@ -47,6 +52,42 @@ app.post('/block',async (req,res)=>{
     //     .then((bl)=>{
     //         res.json(bl)
     //     })
+
+});
+
+// user provides public address in POST request, and receives message to sign 
+let memPool = {};
+app.post('/requestValidation',(req,res)=>{
+    let address = req.body.address;
+    let requestTimeStamp = new Date().getTime().toString().slice(0,-3);
+    let message = `${address}:${requestTimeStamp}:starRegistry`;
+    const validationRequest = {address,requestTimeStamp,message,"validationWindow":300}
+    memPool[address] = {address,requestTimeStamp,message};  
+    res.json(validationRequest)
+})
+
+// POST request with address, signature, and message to verify identity 
+app.post('/message-signature/validate',(req,res)=>{
+    const{address,signature,message} = req.body; 
+    if(memPool[address]) {
+        let timeNow = new Date().getTime().toString().slice(0,-3);
+        let timeWindow = timeNow-memPool[address].requestTimeStamp-300;
+        if(timeWindow > 0){
+            delete memPool[address];
+            res.json('Waiting time exceeded 5 minutes; try again, please!')
+        } else {
+            const verificationResponse ={
+                messageSigniture: bitcoinMessage.verify(message, address, signature),
+                address,
+                signature,
+                message,
+                validationWindow:-timeWindow,
+                requestTimeStamp:memPool[address].requestTimeStamp                   
+            };
+            res.json(verificationResponse);
+        }
+    }
+    res.json('There is no such address.');
 
 })
 
